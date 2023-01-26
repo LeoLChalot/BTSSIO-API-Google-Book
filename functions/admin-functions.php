fdf78155b92eb16f88c8cdd8d68f1cc3dcdfe246
LeoLChalot <larouchalotleo@gmail.com>
2023-01-26 08:54:20 +0100

    ok

--- functions/admin-functions.php
+++ functions/admin-functions.php
@@ -4,21 +4,27 @@ if (!empty($_GET['func'])) {
     $function = $_GET['func'];
     if (!empty($_SESSION) && ($_SESSION['role'] == 'admin')) {
         switch ($function) {
+                // ! FT USERDELETE
             case 'userDelete':
-                    $userID = $_GET['id'];
-                    $sth_userDelete = $connexion->prepare("DELETE FROM users WHERE id = $userID");
-                    $sth_userDelete->execute();
-                    header('location: ../admin_dashboard.php');
+                $userID = $_GET['id'];
+                $sth_userDelete = $connexion->prepare("DELETE FROM users WHERE id = $userID");
+                $sth_userDelete->execute();
+                header('location: ../admin_dashboard.php');
                 break;
+                // ! END FT USERDELETE
+                // ! FT USEREDIT
             case 'userEdit':
                 header('location: ../admin_dashboard.php?func=listUsers');
                 break;
+                // ! END FT USEREDIT
+                // ! FT MSGDELETE
             case 'msgDelete':
-                    $msgID = $_GET['id'];
-                    $sth_userDelete = $connexion->prepare("DELETE FROM messages WHERE id = $msgID");
-                    $sth_userDelete->execute();
-                    header('location: ../admin_dashboard.php');
+                $msgID = $_GET['id'];
+                $sth_userDelete = $connexion->prepare("DELETE FROM messages WHERE id = $msgID");
+                $sth_userDelete->execute();
+                header('location: ../admin_dashboard.php');
                 break;
+                // ! END FT MSGDELETE
             default:
                 header('location: ../index.php');
                 break;
