30a1c8819176e2e039bf626b4ed3f200bda78ef8
LeoLChalot <larouchalotleo@gmail.com>
2023-03-21 23:19:19 +0100

    SCRIPT | Doc | OK

--- cURL_request.php
+++ cURL_request.php
@@ -5,69 +5,206 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Documentation</title>
+    <style>
+        *,
+        ::before,
+        ::after {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            gap: 1em;
+            justify-content: center;
+            align-items: center;
+        }
+
+        table {
+            border: 1px solid #b3adad;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+
+        table th {
+            border: 1px solid #b3adad;
+            padding: 5px;
+            background: #f0f0f0;
+            color: #313030;
+        }
+
+        table td {
+            border: 1px solid #b3adad;
+            text-align: center;
+            padding: 10px 5px;
+            background: #ffffff;
+            color: #313030;
+        }
+    </style>
 </head>
 
 <body>
-    <?php
-    $key = '$AIzaSyAY1N3MiifNN02kmk2X6j64tk6WVP57kqQ';
-    $title = "Javascript";
+    <div id="input">
+        <form action="" method="get">
+            <input type="text" name="title" id="title">
+            <input type="submit" value="Chercher">
+        </form>
 
-    $url = "https://www.googleapis.com/books/v1/volumes?q=$title&langRestrict=fr";
-    $curl = curl_init($url);
-    $options = array(
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => array('Content-type: application/json'),
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_URL => $url,
-        CURLOPT_CAINFO => __DIR__ . '/assets/Certificat/GTS Root R1.crt'
-    );
-
-    curl_setopt_array($curl, $options);
-
-
-
-    $resp = curl_exec($curl);
-
-    if ($e = curl_error($curl)) {
-        var_dump($e);
-    } else {
-        $data = json_decode($resp, true);
-        $results = $data["items"];
-        // var_dump($results);
-        // var_dump($results[0]['volumeInfo']['imageLinks']);
-    }
-
-    for ($i = 0; $i < count($results); $i++) {
-        $booksId = $results[$i]['id'];
-        if (isset($results[$i]["volumeInfo"]["title"])) {
-            echo "Titre : " . $results[$i]["volumeInfo"]["title"] . "<br>";
-        }
-        if (isset($results[$i]["volumeInfo"]["subtitle"])) {
-            echo "Sous-titre : " . $results[$i]["volumeInfo"]["subtitle"] . "<br>";
-        }
-        if (isset($results[$i]["volumeInfo"]["description"])) {
-            echo "Description : " . $results[$i]["volumeInfo"]["description"] . "<br>";
-        }
-        if (isset($results[$i]["volumeInfo"]["authors"])) {
-            echo "Auteur(s) : ";
-            foreach ($results[$i]["volumeInfo"]["authors"] as $author) {
-                echo $author . ", ";
-            }
-        }
-        if (isset($results[$i]["volumeInfo"]['imageLinks'])) : ?>
+    </div>
+    <div class="cURL">
+        <?php
+        if (!empty($_GET['title'])) : ?>
             <?php
-            // var_dump($results[$i]["volumeInfo"]['imageLinks']);
-            $imgLink = $results[$i]["volumeInfo"]['imageLinks']['smallThumbnail']; ?>
-            <img src="<?= $imgLink ?>" alt="">
-        <?php endif ?>
-    <?php echo "<br><br>";
-    }
+            $key = '$AIzaSyAY1N3MiifNN02kmk2X6j64tk6WVP57kqQ';
+            $title = $_GET['title'];
+            $title = str_replace(' ', '+', $title);
 
+            $url = "https://www.googleapis.com/books/v1/volumes?q=$title&langRestrict=fr&maxResults=1";
+            $curl = curl_init($url);
+            $options = array(
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => array('Content-type: application/json'),
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_URL => $url,
+                CURLOPT_CAINFO => __DIR__ . '/assets/Certificat/GTS Root R1.crt'
+            );
 
+            curl_setopt_array($curl, $options);
+            $resp = curl_exec($curl);
+            if ($e = curl_error($curl)) {
+                var_dump($e);
+            } else {
+                $data = json_decode($resp, true);
+                $results = $data["items"];
+                // var_dump($data);
+            }
+            ?>
+            <table>
+                <thead>
+                    <tr id="titre-table">
+                        <th colspan="2">$results[$i]['volumeInfo']</th>
+                    </tr>
+                </thead>
+                <tbody id="global-items">
+                    <tr id="book-id">
+                        <td>["id"]</td>
+                        <td><?= $results[0]["id"] ?></td>
+                    </tr>
+                    <tr id="book-title">
+                        <td>["title"]</td>
+                        <td><?php
+                            if (isset($results[0]["volumeInfo"]["title"])) {
+                                echo $results[0]["volumeInfo"]["title"];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-subtitle">
+                        <td>["subtitle"]</td>
+                        <td><?php
+                            if (isset($results[0]["volumeInfo"]["subtitle"])) {
+                                echo $results[0]["volumeInfo"]["subtitle"];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-authors">
+                        <td>["authors"]</td>
+                        <td>
+                            <?php
+                            if (isset($results[0]["volumeInfo"]["authors"])) {
+                                foreach ($results[0]["volumeInfo"]["authors"] as $author) {
+                                    echo $author . ", ";
+                                }
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-publisher">
+                        <td>["publisher"]</td>
+                        <td><?php
+                            if (isset($results[0]["volumeInfo"]["publisher"])) {
+                                echo $results[0]["volumeInfo"]["publisher"];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-publishedDate">
+                        <td>["publishedDate"]</td>
+                        <td><?php
+                            if (isset($results[0]["volumeInfo"]["publishedDate"])) {
+                                echo $results[0]["volumeInfo"]["publishedDate"];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-description">
+                        <td>["description"]</td>
+                        <td>Description du livre</td>
+                    </tr>
+                    <tr id="book-pageCount">
+                        <td>["pageCount"]</td>
+                        <td><?php
+                            if (isset($results[0]["volumeInfo"]["pageCount"])) {
+                                echo $results[0]["volumeInfo"]["pageCount"];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-printType">
+                        <td>["printType"]</td>
+                        <td><?php
+                            if (isset($results[0]["volumeInfo"]["printType"])) {
+                                echo $results[0]["volumeInfo"]["printType"];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-previewLink">
+                        <td>["previewLink"]</td>
+                        <td><?php
+                            if (isset($results[0]["volumeInfo"]["previewLink"])) {
+                                echo $results[0]["volumeInfo"]["previewLink"];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-infoLink">
+                        <td>["infoLink"]</td>
+                        <td><?php
+                            if (isset($results[0]["volumeInfo"]["infoLink"])) {
+                                echo $results[0]["volumeInfo"]["infoLink"];
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr id="book-imageLinks">
+                        <td>["imageLinks"]["smallThumbnail"]</td>
+                        <td>
+                            <?php
+                            if (isset($results[0]["volumeInfo"]['imageLinks']['smallThumbnail'])) {
+                                echo $results[0]["volumeInfo"]['imageLinks']['smallThumbnail'];
+                            } ?>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
-    curl_close($curl);
-    ?>
+        <?php
+        endif
+        ?>
+    </div>
+    <a href="index.php"><button type="button" class="btn btn-success">Retour</button></a>
+
+    <?php
+    if (isset($curl)) {
+        curl_close($curl);
+    } ?>
 </body>
 
 </html>
--- catalogue.php
+++ catalogue.php
@@ -1,19 +1,40 @@
 <?php require_once(__DIR__ . '/require/bdd-on.php'); ?>
 <?php require_once(__DIR__ . '/require/header.php');
-// define('APIKEY', '$AIzaSyAY1N3MiifNN02kmk2X6j64tk6WVP57kqQ');
 ?>
-<section class="text-center text-lg-start">
-    <style>
-        .cascading-right {
-            margin-left: -50px;
-        }
+<style>
+    .cascading-right {
+        margin-left: -50px;
+    }
 
-        @media (max-width: 992px) {
-            .cascading-right {
-                margin-right: 0;
-            }
+    @media (max-width: 992px) {
+        .cascading-right {
+            margin-right: 0;
         }
-    </style>
+    }
+
+    .card-container {
+        width: 18rem;
+        transition: .3s all ease;
+    }
+
+    .card-container:hover {
+        z-index: 999;
+        transform: scale(1.1);
+    }
+
+    .card-container img {
+        transition: .3s all ease;
+    }
+
+    .card-container:hover img {
+        transform: rotate(10deg);
+    }
+
+    li {
+        list-style-type: none;
+    }
+</style>
+<section class="text-center text-lg-start">
     <div class="container py-4">
         <div class="row g-0 align-items-center justify-content-center">
             <div class="col-lg-5 mb-6 mb-lg-0">
@@ -33,39 +54,108 @@
                 </div>
             </div>
 
+
             <?php if (!empty($_GET['title'])) : ?>
                 <?php
                 $title = $_GET['title'];
+                $title = str_replace(' ', '+', $title);
+                $url = "https://www.googleapis.com/books/v1/volumes?q=$title&langRestrict=fr&maxResults=3";
+                $curl = curl_init($url);
+                $options = array(
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_HTTPHEADER => array('Content-type: application/json'),
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_URL => $url,
+                    CURLOPT_CAINFO => __DIR__ . '/assets/Certificat/GTS Root R1.crt'
+                );
+
+                curl_setopt_array($curl, $options);
+
+                $resp = curl_exec($curl);
+
+                if ($e = curl_error($curl)) {
+                    var_dump($e);
+                } else {
+                    $data = json_decode($resp, true);
+                    $results = $data["items"];
+                }
                 ?>
-                <div class="center col-lg-8 mt-5 mb-5 mb-lg-0">
-                    <div class="card cascading-right shadow-sm" style="background: hsla(0, 0%, 100%, 0.55);backdrop-filter: blur(30px);">
-                        <div class="card-body p-5 shadow-5 text-center d-flex flex-column align-items-center">
-                            <h3>Liste des livres trouvés</h3>
-                            <?php
-                            $googleBook = new BookSearcher();
-                            // ? Recherche par mot clés //
-                            $livres = $googleBook->getBooksByKeyword($title);
-                            ?>
-                            <?php for ($i = 0; $i < count($livres); $i++) : ?>
-                                <div class="book-card col-12 d-flex gap-5 m-4 p-4 border-bottom d-flex flex-row justify-content-center align-items-center">
-                                    <?php if (isset($livres[$i]['image'])) : ?>
-                                        <div class="book-img">
-                                            <img src="<?= $livres[$i]['image'] ?>" alt="">
-                                        </div>
+
+                <div class="center col-md-12 d-flex justify-content-center py-5 flex-wrap gap-4">
+
+                    <?php for ($i = 0; $i < count($results); $i++) : ?>
+
+                        <div class="card card-container shadow">
+                            <div class="card-header d-flex justify-content-center align-items-center" style="height: 15rem;">
+                                <?php if (isset($results[$i]["volumeInfo"]['imageLinks']['thumbnail'])) : ?>
+                                    <img class="card-img-top center" src="<?= $results[$i]["volumeInfo"]['imageLinks']['smallThumbnail'] ?>" style="width:130px;" alt="">
+                                <?php else : ?>
+                                    <img class="card-img-top center" src="assets/img/backgrounds/empty_book.png" style="width:200px;" alt="">
+                                <?php endif ?>
+                            </div>
+                            <div class="card-body d-flex flex-column justify-content-between">
+                                <div class="book-title">
+                                    <h5><?= $results[$i]["volumeInfo"]["title"] ?></h5>
+                                    <?php if (isset($results[$i]["volumeInfo"]["subtitle"])) : ?>
+                                        <p class="card-text"><?= $results[$i]["volumeInfo"]["subtitle"] ?></p>
                                     <?php endif ?>
-                                    <div class="book-info d-flex flex-column gap-3 justify-content-center align-items-center">
-                                        <p><strong><?= $livres[$i]['titre'] ?></strong></p>
-                                        <p><strong>Auteur :</strong> '<?= $livres[$i]['auteur'] ?></p>
-                                    </div>
                                 </div>
-                            <?php endfor ?>
+                                <div class="book-auteur">
+                                    <?php if (isset($results[$i]["volumeInfo"]["authors"])) : ?>
+                                        <p><strong>Auteur(s) :</strong></p>
+                                        <ul>
+                                            <?php foreach ($results[$i]["volumeInfo"]["authors"] as $author) : ?>
+                                                <li><?= $author ?></li>
+                                            <?php endforeach ?>
+                                        </ul>
+                                    <?php endif ?>
+                                </div>
+                                <div class="book-description text-justify">
+                                    <?php if (isset($results[$i]["volumeInfo"]["description"])) : ?>
+                                        <details>
+                                            <summary>Details</summary>
+                                            <p><?= $results[$i]["volumeInfo"]["description"]; ?></p>
+                                        </details>
+                                    <?php endif ?>
+                                </div>
+                            </div>
+                            <div class="card-footer d-flex justify-content-around">
+                                <a class="btn btn-primary" href="<?= $results[$i]['volumeInfo']['previewLink'] ?> " target="_blank">Lire</a>
+                                <?php if (!empty($_SESSION)) : ?>
+                                    <a class="btn btn-success" href="http://">Ajouter</a>
+                                <?php endif ?>
+
+                            </div>
                         </div>
-                    </div>
+                    <?php endfor ?>
                 </div>
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item disabled">
+                            <a class="page-link">Previous</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+
+
+
             <?php endif ?>
+
+
+            <?php
+            if (isset($curl)) {
+                curl_close($curl);
+            } ?>
 
         </div>
     </div>
+
 </section>
 <?php require_once(__DIR__ . '/require/footer.php'); ?>
 <?php require_once(__DIR__ . '/require/bdd-off.php'); ?>
